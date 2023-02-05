bc432664ee0e11f6e44ebbd3b8f34a5dd7c99694
brunnarayssa <brunnarayssa@hotmail.es>
2023-02-05 14:09:28 +0000

    cambios en l hoja de estilos y en el home

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,81 +1,8 @@
-import React, { useState }  from "react";
+import React from "react";
 
 //create your first component
 const Home = () => {
-
-	let [secondsUni, setSecondsUni] = useState(0);
-	let [secondsDec, setSecondsDec] = useState(0);
-	let [minutesUni, setMinutesUni] = useState(0);
-	let [minutesDec, setMinutesDec] = useState(0);
-	let [hoursUni, setHoursUni] = useState(0);
-	let [hoursDec, setHoursDec] = useState(0);
-
-	const change = () => {
-		setSecondsUni(secondsUni += 1);
-
-		if(secondsUni === 10){
-			setSecondsUni(secondsUni = 0);
-			setSecondsDec(secondsDec += 1);
-		};
-	
-		if(secondsDec === 6){
-			setSecondsDec(secondsDec = 0);
-			setSecondsUni(secondsUni = 0);
-			setMinutesUni(minutesUni += 1);
-		};
-	
-		if(minutesUni === 10){
-			setMinutesUni(minutesUni = 0);
-			setSecondsDec(secondsDec = 0);
-			setSecondsUni(secondsUni = 0);
-			setMinutesDec(minutesDec += 1);
-		};
-	
-		if(minutesDec === 6){
-			setMinutesDec(minutesDec = 0);
-			setMinutesUni(minutesUni = 0);
-			setSecondsDec(secondsDec = 0);
-			setSecondsUni(secondsUni = 0);
-			setHoursUni(hoursUni += 1);
-		};
-	
-		if(hoursUni === 10){
-			setHoursUni(hoursUni = 0);
-			setMinutesDec(minutesDec = 0);
-			setMinutesUni(minutesUni = 0);
-			setSecondsDec(secondsDec = 0);
-			setSecondsUni(secondsUni = 0);
-			setHoursDec(hoursDec += 1);
-		};
-	
-		if(hoursDec == 6){
-			setHoursDec(hoursDec = 0);
-			setHoursUni(hoursUni = 0);
-			setMinutesDec(minutesDec = 0);
-			setMinutesUni(minutesUni = 0);
-			setSecondsDec(secondsDec = 0);
-			setSecondsUni(secondsUni = 0);
-		};
-
-
-	}
-
-	setInterval(change, 3000);
-
-	return (
-		<div className="wrapper">
-			<div className="full-clock">
-				<p className="digit"><span class="material-symbols-outlined"> <i class="fa-regular fa-clock"></i></span></p>
-				<p className="digit">{hoursDec}</p>
-				<p className="digit">{hoursUni}</p>
-				<p className="digit">{minutesDec}</p>
-				<p className="digit">{minutesUni}</p>
-				<p className="digit">{secondsDec}</p>
-				<p className="digit">{secondsUni}</p>
-					
-			</div>
-		</div>
-	);
+	return 
 };
 
 export default Home;
